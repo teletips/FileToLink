6e0ab6b0484f8be44691a41d019d12b1ff8c0548
Thakshaka Rathnayake <76155498+Thakshaka@users.noreply.github.com>
2021-05-09 15:08:32 +0530

    Delete presets.py

--- presets.py
+++
@@ -1,6 +0,0 @@
-class Presets(object):
-    WELCOME_TEXT = "Heya, <b>{}</b>\n<code>I am a File To Link Bot created by TeLe TiPs\nSend me any file & I will give you the shorten link of it👇🏻</code> "
-
-
-# Thakshaka Rathnayake - TeLe TiPs
-# ©️2021 TeLe TiPs All Rights Reserved 
