e4a9723416bc0a5b05aaf6779c93b5281a11bbd4
Thakshaka Rathnayake <76155498+Thakshaka@users.noreply.github.com>
2021-05-09 16:29:05 +0530

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,42 @@
+import telebot
+import time
+import pyshorteners
+
+bot_token = '1724266823:AAEKPicuKYlxWo9SALl3sfTrkCsySpih8N4'
+
+bot = telebot.TeleBot(token=bot_token)
+
+def short(url):
+    return pyshorteners.Shortener().tinyurl.short(url)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'Hello, welcome!')
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, 'Read my description for further help')  
+
+@bot.message_handler(content_types=['photo', 'video', 'audio', 'document'])
+def file_sent(message):
+    try:
+        bot.send_message(message.chat.id, short(bot.get_file_url(message.document.file_id)))
+    except AttributeError:
+        try:
+            bot.send_message(message.chat.id, short(bot.get_file_url(message.photo[0].file_id)))
+        except AttributeError:
+            try:
+                bot.send_message(message.chat.id, short(bot.get_file_url(message.audio.file_id)))
+            except AttributeError:
+                try:
+                    bot.send_message(message.chat.id, short(bot.get_file_url(message.video.file_id)))
+                except AttributeError:
+                    pass
+
+
+while True:
+    try:
+        bot.polling()
+    except Exception:
+        time.sleep(15)
+
